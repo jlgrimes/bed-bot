6d18f5effae1ba55d794de836e17f5fe50254f99
Jared Grimes <jaredlgrimes@hotmail.com>
2020-05-10 00:37:56 -0400

    stats-update-all complete

--- bot.js
+++ bot.js
@@ -3,9 +3,17 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
+const schedule = require('node-schedule');
 const servRoles = require('./roles')
+const storage = require('node-persist');
 
 client.on('ready', () => {
+    storage.init();
+
+    // every day at midnight
+    let job = schedule.scheduleJob('0 0 * * *', () => {
+        console.log('The answer to life, the universe, and everything!');
+    });
     console.log(`Logged in as ${client.user.tag}!`);
 });
 client.login(process.env.DISCORD_TOKEN);
@@ -17,12 +25,87 @@ const bwStatsUrl = (name) => apiBase + 'player/' + name + '/BED';
 const winRate = (data) => data.victories / data.games_played;
 const kd = (data) => data.kills / data.deaths;
 
+const trimMention = (mention) => mention
+    .replace('<@', '')
+    .replace('>', '')
+    .replace('!', '');
+
+const addServRoles = (msg, mention, ign) => {
+    fetch(bwStatsUrl(ign))
+    .then((response) => response.json())
+    .then(data => {
+        msg.guild.roles.fetch()
+            .then(roles => {
+                let servKdRole = servRoles.kd.filter(kdRole => kdRole.range(kd(data)))
+                let servKdRoleName = servKdRole[0].name
+
+                let kdRole = roles.cache.filter(role => role.name === servKdRoleName)
+                if (kdRole.size === 0) {
+                    msg.reply('No roles named ' + name)
+                    return;
+                }
+                kdRole = kdRole.values().next().value
+
+                let servWrRole = servRoles.wr.filter(wrRole => wrRole.range(winRate(data)))
+                let servWrRoleName = servWrRole[0].name
+
+                let wrRole = roles.cache.filter(role => role.name === servWrRoleName)
+                if (wrRole.size === 0) {
+                    msg.reply('No roles named ' + name)
+                    return;
+                }
+                wrRole = wrRole.values().next().value
+
+                msg.guild.members.fetch(mention)
+                    .then((member) => {
+                        const allServRoleNames = [...servRoles.kd.map(role => role.name), ...servRoles.wr.map(role => role.name)];
+                        const allServRoles = roles.cache.filter(role => allServRoleNames.includes(role.name))
+                        member.roles.remove(allServRoles)
+                            .then(() => {
+                                member.roles.add([wrRole, kdRole])
+                                    .catch((err) => console.log(err))
+                                    .then(() => msg.reply('Roles ' + servWrRoleName + ' and ' + servKdRoleName + ' added for ' + member.user.username + '!'))
+                            })         
+                    })
+            });
+    })
+}
+
 client.on('message', msg => {
     let message = msg.content;
     if (message.substring(0, 1) == '!') {
         let args = message.substring(1).split(' ');
         let cmd = args[0];
         args = args.splice(1);
+
+        if (cmd === 'set-ign') {
+            if (args.length !== 2) {
+                msg.reply('Usage: !set-ign [mentioned user] [ign]')
+                return;
+            }
+            storage.setItem(args[0], args[1])
+                .then(() =>  msg.reply(args[0] + ', ' + args[1] + ' stored!'))
+        }
+
+        if (cmd === 'get-ign') {
+            storage.getItem(args[0])
+                .then(item => console.log(item))
+        }
+
+        if (cmd === 'remove-ign') {
+            storage.removeItem(args[0])
+                .then(() =>  msg.reply(args[0] + ' removed!'))
+        }
+
+        if (cmd === 'list-ign') {
+            storage.forEach(async (datum) => {
+                msg.reply(datum.key + ", " + datum.value)
+            });
+        }
+
+        if (cmd === 'wipe-ign') {
+            storage.clear().then(() => msg.reply('Database cleared!'))
+        }
 
         if (cmd === 'stats') {
             let name = args[0];
@@ -56,51 +139,22 @@ client.on('message', msg => {
             //member.addRole(role).catch(console.error);
         }
 
+        if (cmd === 'stats-update-all') {
+            storage.forEach(async (datum) => {
+                let mention = trimMention(datum.key);
+                let ign = datum.value;
+                addServRoles(msg, mention, ign);
+            });
+        }
+
         if (cmd === 'stats-update') {
             if (args.length !== 2) {
                 msg.reply('Usage: !stats-update [mentioned user] [ign]')
                 return;
             }
-            let mention = args[0]
-                .replace('<@', '')
-                .replace('>', '')
-                .replace('!', '');
+            let mention = trimMention(args[0]);
             let ign = args[1];
-
-            fetch(bwStatsUrl(ign))
-                .then((response) => response.json())
-                .then(data => {
-                    msg.guild.roles.fetch()
-                        .then(roles => {
-                            let servKdRole = servRoles.kd.filter(kdRole => kdRole.range(kd(data)))
-                            let servKdRoleName = servKdRole[0].name
-
-                            let kdRole = roles.cache.filter(role => role.name === servKdRoleName)
-                            if (kdRole.size === 0) {
-                                msg.reply('No roles named ' + name)
-                                return;
-                            }
-                            kdRole = kdRole.values().next().value
-
-                            let servWrRole = servRoles.wr.filter(wrRole => wrRole.range(winRate(data)))
-                            let servWrRoleName = servWrRole[0].name
-
-                            let wrRole = roles.cache.filter(role => role.name === servWrRoleName)
-                            if (wrRole.size === 0) {
-                                msg.reply('No roles named ' + name)
-                                return;
-                            }
-                            wrRole = wrRole.values().next().value
-        
-                            msg.guild.members.fetch(mention)
-                                .then((member) => {
-                                    member.roles.add([wrRole, kdRole])
-                                        .catch((err) => console.log(err))
-                                        .then(() => msg.reply('Roles added!'))
-                                    
-                                })
-                        });
-                })
+            addServRoles(msg, mention, ign);
         }
      }
 });
