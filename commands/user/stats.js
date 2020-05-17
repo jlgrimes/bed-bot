d88077b96fc83ac7fad7137f7ca193fd79023320
Jared Grimes <jaredlgrimes@hotmail.com>
2020-05-17 18:59:17 -0400

    new format for stats output

--- commands/user/stats.js
+++ commands/user/stats.js
@@ -49,31 +49,35 @@ module.exports = class DatabaseWipeCommand extends Command {
                 let wr = (100 * data.victories / data.gamesPlayed).toFixed(3)
                 let kd = (data.kills / data.deaths).toFixed(3)
 
-                let reply = '\n';
+                let reply = '\n```';
 
                 if (mode) {
-                    reply += '**' + mode[0].toUpperCase() + mode.slice(1) + '** s'
+                    reply += mode[0].toUpperCase() + mode.slice(1) + ' s'
                 }
                 else {
                     reply += 'S'
                 }
 
-                reply += 'tats for ' + ign + ':\n' +
-                    'Points:\t**' + data.points + '**\n' +
-                    'Last Login:\t**' + data.lastLogin.toLocaleString('en-US', { timeZoneName: 'short' }) + '**\n' +
-                    'Victories:\t**' + data.victories + '**\n' +
-                    'Games Played:\t**' + data.gamesPlayed + '**\n' +
-                    'Win Rate:\t**' + wr + '%**\n' +
-                    'Kills:\t**' + data.kills + '**\n' +
-                    'Deaths:\t**' + data.deaths + '**\n' +
-                    'KD:\t**' + kd + '**\n' +
-                    'Beds Destroyed:\t**' + data.bedsDestroyed + '**\n' +
-                    'Teams Eliminated:\t**' + data.teamsEliminated + '**\n' +
-                    'Win Streak:\t**' + data.winStreak + '**\n'
+                reply += 'tats for ' + ign + '\n' +
+                    '\n' +
+                    'Points:           ' + data.points + '\n' +
+                    'Last Login:       ' + data.lastLogin.toLocaleString('en-US', { timeZoneName: 'short' }) + '\n' +
+                    'Victories:        ' + data.victories + '\n' +
+                    'Games Played:     ' + data.gamesPlayed + '\n' +
+                    'Win Rate:         ' + wr + '%\n' +
+                    'Kills:            ' + data.kills + '\n' +
+                    'Deaths:           ' + data.deaths + '\n' +
+                    'KD:               ' + kd + '\n' +
+                    'Beds Destroyed:   ' + data.bedsDestroyed + '\n' +
+                    'Teams Eliminated: ' + data.teamsEliminated + '\n' +
+                    'Win Streak:       ' + data.winStreak + '\n'
 
                 if (!mode) {
-                    reply += 'Title:\t**' + data.title + '**'
+                    reply += 
+                    'Title:            ' + data.title
                 }
+
+                reply += '```'
                 
                 message.reply(reply)
             })
