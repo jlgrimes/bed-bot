f95a0aa57c5e7a82bb8a8045d0583963a56a3411
Jared Grimes <jaredlgrimes@hotmail.com>
2020-05-27 15:42:04 -0400

    adding roles based off

--- bot.js
+++ bot.js
@@ -26,3 +26,33 @@ client.login(process.env.DISCORD_TOKEN);
 client.on('message', (message) => {
     //...
 });
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+	// Now the message has been cached and is fully available
+	// console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+   //  console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    
+    const guild = client.guilds.resolve(process.env.SERVER_ID);
+    const roles = await guild.roles.fetch();
+
+    const roleName = reaction.message.content.split('`')[1]
+    const targetRole = roles.cache.filter(
+        (role) => role.name === roleName
+    );
+
+    const member = await guild.members.fetch(user.id)
+    member.roles.add(targetRole)
+    console.log("role added")
+});
